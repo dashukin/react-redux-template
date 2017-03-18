4a3394666ad3089ea41574ad307ac6170905964a
dashukin <dashukin@gmail.com>
2017-03-18 17:26:14 +0300

    Create constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,15 @@
+/*
+* Constants
+* */
+
+import keymirror from 'keymirror';
+
+const constants = keymirror({
+
+});
+
+const values = {
+
+};
+
+export default Object.assign({}, constants, values);
