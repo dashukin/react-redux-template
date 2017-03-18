3a814dac25a1788f8353123115fdf3e8ce47a167
dashukin <dashukin@gmail.com>
2017-03-18 18:15:07 +0300

    Add example constant values

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,11 +5,11 @@
 import keymirror from 'keymirror';
 
 const constants = keymirror({
-
+	EXAMPLE_1: null
 });
 
 const values = {
-
+	EXAMPLE_2: 'example-2'
 };
 
 export default Object.assign({}, constants, values);
