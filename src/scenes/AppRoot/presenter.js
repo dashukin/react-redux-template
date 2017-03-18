2fdd2802ef12e1e0fe339f36fc95d14b7128fe9f
dashukin <dashukin@gmail.com>
2017-03-19 00:43:50 +0300

    Add image to example component

--- src/scenes/AppRoot/presenter.js
+++ src/scenes/AppRoot/presenter.js
@@ -3,11 +3,15 @@
 * */
 
 import React from 'react';
+import imgSrc from './images/redux.png';
 
 const AppRoot = props => {
 	return (
 		<div className="application">
-
+			Example component
+			<div className="application__logo">
+				<img src={imgSrc} alt=""/>
+			</div>
 		</div>
 	);
 };
