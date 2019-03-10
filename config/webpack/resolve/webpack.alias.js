4b0457bff69bee00a7dbbfbccdfe5f83231934c8
dashukin <dashukin@gmail.com>
2019-03-10 17:15:18 +0300

    Remove unused aliases

--- config/webpack/resolve/webpack.alias.js
+++ config/webpack/resolve/webpack.alias.js
@@ -7,11 +7,6 @@ export const alias = {
 	components:		path.resolve(APP_SRC_DIR, 'components'),
 	constants:		path.resolve(APP_SRC_DIR, 'constants'),
 	data:			path.resolve(APP_SRC_DIR, 'data'),
-	'app-history':	path.resolve(APP_SRC_DIR, 'history'),
-	middleware: 	path.resolve(APP_SRC_DIR, 'middleware'),
-	models:			path.resolve(APP_SRC_DIR, 'models'),
-	scenes:			path.resolve(APP_SRC_DIR, 'scenes'),
-	scss:			path.resolve(APP_SRC_DIR, 'scss'),
 	services:		path.resolve(APP_SRC_DIR, 'services'),
 	selectors:		path.resolve(APP_SRC_DIR, 'selectors'),
 	utils:			path.resolve(APP_SRC_DIR, 'utils')
