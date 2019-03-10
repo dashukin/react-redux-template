97077e402da73b06cecce1ddde06f42a02621111
dashukin <dashukin@gmail.com>
2019-03-10 17:15:09 +0300

    Update paths

--- config/environment/environment.js
+++ config/environment/environment.js
@@ -18,10 +18,10 @@ export const WEBPACK_MODE = DEV ? WEBPACK_MODE_DEV : WEBPACK_MODE_PROD;
 export const ROOT_DIR = path.resolve(__dirname, '../../');
 
 export const CONFIG_DIR = path.resolve(ROOT_DIR, './config');
-export const POSTCSS_CONFIG_ENTRY = path.resolve(CONFIG_DIR, './postcss/index.js');
+export const POSTCSS_CONFIG_ENTRY = path.resolve(CONFIG_DIR, './postcss/application.js');
 
 export const APP_SRC_DIR = path.resolve(ROOT_DIR, './src');
-export const APP_SRC_ENTRY = path.resolve(APP_SRC_DIR, './index.js');
+export const APP_SRC_ENTRY = path.resolve(APP_SRC_DIR, './application.js');
 export const APP_SRC_TEMPLATE_ENTRY = path.resolve(APP_SRC_DIR, './index.html');
 
 export const DIST_DIR = path.resolve(ROOT_DIR, './dist');
