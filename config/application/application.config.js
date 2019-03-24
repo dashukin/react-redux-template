a1a69cd28eb7980aa40f9044cdf8458ab6c34b4c
dashukin <dashukin@gmail.com>
2019-03-24 12:51:53 +0300

    Add actions logger configuration property

--- config/application/application.config.js
+++ config/application/application.config.js
@@ -1,1 +1,3 @@
 export const APPLICATION_ELEMENT_ID = 'application';
+
+export const LOG_ACTIONS_ENABLED = false;
--- src/client/store/_middleware/logger.middleware.js
+++ src/client/store/_middleware/logger.middleware.js
@@ -1,11 +1,11 @@
 /*
-* Logger middleware
+* Logger _middleware
 * */
 
-import config from 'src/client/config';
+import { LOG_ACTIONS_ENABLED } from 'config/application';
 
 export const loggerMiddleware = store => next => action => {
-	if (config.DEV === true) {
+	if (LOG_ACTIONS_ENABLED) {
 		console.warn(action);
 	}
 
