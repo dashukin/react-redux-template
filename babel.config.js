518c216622a6896201a3b911678e2be4b47c162a
dashukin <dashukin@gmail.com>
2019-04-12 00:07:30 +0300

    Update code style

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-	presets: [
-		'@babel/preset-react',
-		[
-			'@babel/preset-env',
-			{
-				"useBuiltIns": "entry",
-				"targets": {
-					"browsers": [
-						"last 2 versions", "safari >= 7"
-					]
-				}
-			}
-		]
-	],
-	plugins: [
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-syntax-dynamic-import',
-		'@babel/plugin-proposal-object-rest-spread'
-	]
-}
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+        targets: {
+          browsers: [
+            'last 2 versions', 'safari >= 7',
+          ],
+        },
+      },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+};
