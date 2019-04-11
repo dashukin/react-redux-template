759494ea5baef3937cfdaab86d125fb2b9d03861
dashukin <dashukin@gmail.com>
2019-04-11 23:34:20 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,16 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: 'airbnb',
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings'
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    PRODUCTION: 'readonly',
+    SSR: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
@@ -19,5 +25,20 @@ module.exports = {
     'react',
   ],
   rules: {
+    'class-methods-use-this': 'off',
+    'import/namespace': ['on', { allowComputed: true }],
+    'import/no-duplicates': 'off',
+    'import/prefer-default-export': 'off',
+    'eact/jsx-filename-extension': 'off',
+    'no-underscore-dangle': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prefer-stateless-function': 'off'
   },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './config/webpack/webpack.config.babel.js'
+      }
+    }
+  }
 };
